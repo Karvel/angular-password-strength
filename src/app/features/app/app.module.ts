f06e2344e48ee37fc6a607874d8f8d0ab87a2691
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 16:43:10 -0700

    chore, feat(app): set up `SharedModule` (#6)
    * refactor(app): move files into features directory
    * chore(package, yarn): add material dependencies
    * feat(app): wire up styles, fonts, animations
    * feat(shared): add `MaterialModule`
    * feat(shared): wire up `MaterialModule` and other shared dependencies
    * feat(app): wire up `SharedModule`
    * feat(not-found): add material styling
    * refactor(styles): move material custom theme

--- src/app/features/app/app.module.ts
+++ src/app/features/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
   AppRoutingModule,
   components as mainComponents,
 } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from '../../infrastructure/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent, mainComponents],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
