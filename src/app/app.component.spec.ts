59e942d7e8c8ab0533370d162df8826e73510e37
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 14:41:54 -0700

    test(app): use spectator, add tests for `NotFoundComponent`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,24 @@
-import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
+
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
+describe('[Integration] AppComponent', () => {
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    imports: [RouterTestingModule],
   });
 
+  beforeEach(() => (spectator = createComponent()));
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 
   it(`should have as title 'angular-password-strength'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-password-strength');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'angular-password-strength app is running!'
-    );
+    const expectedValue = 'angular-password-strength';
+    expect(spectator.component.title).toEqual(expectedValue);
   });
 });
