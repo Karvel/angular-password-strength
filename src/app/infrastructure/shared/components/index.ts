46b72cba3eef4152a19b94cd82dc164062b89fbb
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 21:24:28 -0700

    feat(shared): wire up `components`

---
+++ src/app/infrastructure/shared/components/index.ts
@@ -0,0 +1,6 @@
+import { PasswordStrengthComponent } from './password-strength/password-strength.component';
+
+/**
+ * Add components that do not need to be specifically referenced.
+ */
+export const components = [PasswordStrengthComponent];
