9f6b3d790ebc2662714e5bab985273750e0ed843
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 19:46:26 -0700

    feat(register): subscribe to password, assign values, bind to template

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup } from '@angular/forms';
+
+import { Subscription } from 'rxjs';
+
+import { CONSTANTS } from 'src/app/infrastructure/utils/constants';
 
 @Component({
   templateUrl: './register.component.html',
@@ -9,18 +13,63 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   public form: FormGroup = new FormGroup({});
 
+  private subscriptions: Subscription[] = [];
+
   constructor(private fb: FormBuilder) {}
 
   public ngOnInit(): void {
     this.form = this.createForm();
+    this.setupConditionalValidators();
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 
   private createForm(): FormGroup {
     const form = this.fb.group({
       email: [''],
       password: [''],
+      passwordMin: { value: false, disabled: true },
+      passwordDigit: { value: false, disabled: true },
+      passwordSpecial: { value: false, disabled: true },
     });
 
     return form;
+  }
+
+  private get password(): AbstractControl {
+    return this.form.get('password') as AbstractControl;
+  }
+
+  private get passwordMin(): AbstractControl {
+    return this.form.get('passwordMin') as AbstractControl;
+  }
+
+  private get passwordDigit(): AbstractControl {
+    return this.form.get('passwordDigit') as AbstractControl;
+  }
+
+  private get passwordSpecial(): AbstractControl {
+    return this.form.get('passwordSpecial') as AbstractControl;
+  }
+
+  /** Listens to the password input in the form and updates the requirements list. */
+  private setupConditionalValidators(): void {
+    const passwordControlSubscription: Subscription = this.password.valueChanges.subscribe(
+      (controlValue: string) => {
+        controlValue.length >= 8
+          ? this.passwordMin.setValue(true)
+          : this.passwordMin.setValue(false);
+        CONSTANTS.SYMBOL_REGEX.test(controlValue)
+          ? this.passwordSpecial.setValue(true)
+          : this.passwordSpecial.setValue(false);
+        CONSTANTS.DIGIT_REGEX.test(controlValue)
+          ? this.passwordDigit.setValue(true)
+          : this.passwordDigit.setValue(false);
+      }
+    );
+
+    this.subscriptions.push(passwordControlSubscription);
   }
 }
