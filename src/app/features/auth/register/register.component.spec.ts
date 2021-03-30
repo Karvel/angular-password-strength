8c02fafd1ef6d269a5217ee0945aad7a1057101e
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 19:12:28 -0700

    test(register): add test dependencies

--- src/app/features/auth/register/register.component.spec.ts
+++ src/app/features/auth/register/register.component.spec.ts
@@ -1,4 +1,9 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import { MockModule } from 'ng-mocks';
 
 import { RegisterComponent } from './register.component';
 
@@ -6,6 +11,11 @@ describe('[Integration] RegisterComponent', () => {
   let spectator: Spectator<RegisterComponent>;
   const createComponent = createComponentFactory({
     component: RegisterComponent,
+    imports: [
+      ReactiveFormsModule,
+      MockModule(MatCardModule),
+      MockModule(MatFormFieldModule),
+    ],
   });
 
   beforeEach(() => (spectator = createComponent()));
