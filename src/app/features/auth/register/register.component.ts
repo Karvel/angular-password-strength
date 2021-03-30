7d9040e3aa6d5be29cd9727ad0de78755af6d7d3
Elanna Grossman <elanna.grossman@gmail.com>
2021-03-29 18:53:48 -0700

    feat(register): add and bind form

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,8 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  public form: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.form = this.createForm();
+  }
+
+  private createForm(): FormGroup {
+    const form = this.fb.group({
+      email: [''],
+      password: [''],
+    });
+
+    return form;
+  }
+}
